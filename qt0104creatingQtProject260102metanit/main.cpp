920c2da64c0c91942d70fc677dc3d80cb57602b8
CEP.EBC. <CEPEBC@gmail.com>
2026-01-02 22:49:45 +0300

    metanit.com 02.01.2026

---
+++ qt0104creatingQtProject260102metanit/main.cpp
@@ -0,0 +1,13 @@
+// https://metanit.com/cpp/qt/1.6.php
+
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
