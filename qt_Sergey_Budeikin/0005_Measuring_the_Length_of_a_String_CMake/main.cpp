a998c5eb95732e5eff0716a52a3e4ef10811a685
CEP.EBC. <CEPEBC@gmail.com>
2026-03-29 21:28:46 +0300

    2026-03-29 / 0005 Measuring the length of a string in Qt

---
+++ qt_Sergey_Budeikin/0005_Measuring_the_Length_of_a_String_CMake/main.cpp
@@ -0,0 +1,32 @@
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    // Set up code that uses the Qt event loop here.
+    // Call a.quit() or a.exit() to quit the application.
+    // A not very useful example would be including
+    // #include <QTimer>
+    // near the top of the file and calling
+    // QTimer::singleShot(5000, &a, &QCoreApplication::quit);
+    // which quits the application after 5 seconds.
+
+    // If you do not need a running Qt event loop, remove the call
+    // to a.exec() or use the Non-Qt Plain C++ Application template.
+
+    QTextStream out(stdout);
+
+    QString str1 = "Eagle";
+    QString str2 = "Eagle\n";
+    QString str3 = "Eagle ";
+    QString str4 = "Орёл";
+
+    out << str1 << "; " << str1.size() << " = " << str1.count() << " = " << str1.length() << Qt::endl
+        << str2 << "; " << str2.size() << " = " << str2.count() << " = " << str2.length() << Qt::endl
+        << str3 << "; " << str3.size() << " = " << str3.count() << " = " << str3.length() << Qt::endl
+        << str4 << "; " << str4.size() << " = " << str4.count() << " = " << str4.length() << Qt::endl;
+
+
+    return a.exec();
+}
